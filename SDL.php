7baa3803200d5746d14c250cf3e14f309918799d
jessikVv <111167722+jessikVv@users.noreply.github.com>
2022-08-13 00:07:38 -0500

    Add files via upload

---
+++ SDL.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+    <html>
+            <head>
+                <title>SDL</title>
+                <link rel="stylesheet" type="text/css" href="main.css">
+            </head>
+                <body>
+                    <h1>SDL</h1>
+ 
+                    <div class="contenido">
+                            <div>
+                                <h2>¿Que es el SDL?</h2>
+                                <p>Se basa en verificar los requisitos de seguridad a lo largo de las distintas fases de construccion del software: analisis, desarrollo, pruebas y mantenimiento. </p>
+                            </div>
+                                <div><center><img src="img\SDL.png"></center></div>
+                            <div>
+                                <h3>Caracteristicas</h3>
+                                    <ul>
+                                        <li>Modelado de amenazas que sirve a los desarrolladores para encontrar partes del codigo</li>
+                                        <li>La version rigida del SDL es más apropiada para equipos de desarrollo y proyectos mas grandes y no sean susceptibles a cambios durante el proceso</li>
+                                        <li>SDL agil es recomendable para desarrollos de aplicaciones web o basados en la web.</li>
+                                    </ul>
+                            </div>
+                        </div>
+        </body>
+</html>
